1739623b3be5786199b29e0bbae04d52f48600b2
Brian Underwood <public@brian-underwood.codes>
2025-07-31 13:14:33 +0200

    chore: Publish license change

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OtelTelemetryMetrics.MixProject do
   def project do
     [
       app: :otel_telemetry_metrics,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       description: "Temporary library to integration telemetry_metrics with open telemetry",
